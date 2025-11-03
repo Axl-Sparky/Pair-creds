7eb8fca36b4c48d464da8c9aa1a7e5eb1e28737d
Gwak <130032226+Axl-Sparky@users.noreply.github.com>
2025-11-03 20:56:48 +0530

    Refactor index.js to use ES6 module syntax

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require("body-parser");
-const pair = require('./pair');
+import express from 'express';
+import path from 'path';
+import bodyParser from 'body-parser';
+import pair from './pair.js'; // Note the .js extension
+
 const app = express();
 const __path = process.cwd();
 const PORT = process.env.PORT || 7000;
 
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__path, 'public')));
-
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__path, '/pair.html'));
@@ -22,4 +21,4 @@ app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
 
-module.exports = app;
+export default app;
